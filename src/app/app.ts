cdaf12b5420392f5f8cd2e6a7b938cf1ca8418b8
frix241 <ariverosv@unsa.edu.pe>
2025-06-27 17:15:57 -0500

    Añadiendo los campos en donde se guardaran las letras correctas de la palabra actual

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -11,10 +12,17 @@ export class App {
   protected title = 'JuegoAhorcado';
   words: string[] = ['Gato', 'Ahorcado', 'Diccionario', 'Perro', 'Laboratorio', '                    Computadora', 'Compilacion', 'Monitor'];
   selectedWord :string | null = null;
+  letters: string[] = [];
+  guessedLetter: string[] = [];
+  tries: number = 6;
+  gameOver: boolean = false;
 
   getWord() {
     const randIndex = Math.floor(Math.random() * this.words.length);
     this.selectedWord = this.words[randIndex];
+    this.letters = this.selectedWord.split('');
+    this.guessedLetter = [];
+    this.gameOver = false;
   }
     
 
