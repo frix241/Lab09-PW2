bee9d92276bcdd69d9ed3f31c6676b4d9bc21aab
frix241 <ariverosv@unsa.edu.pe>
2025-06-27 17:04:18 -0500

    Creando la función para actualizar la palabra seleccionada.

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected title = 'JuegoAhorcado';
+  words: string[] = ['Gato', 'Ahorcado', 'Diccionario', 'Perro', 'Laboratorio', '                    Computadora', 'Compilacion', 'Monitor'];
+  selectedWord :string | null = null;
+
+  getWord() {
+    const randIndex = Math.floor(Math.random() * this.words.length);
+    this.selectedWord = this.words[randIndex];
+  }
+    
+
+  updateWord(){
+    this.getWord();
+  }
 }
