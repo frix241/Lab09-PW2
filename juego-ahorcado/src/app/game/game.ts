ec3d9dfa9a2c01f7c75573e57b992b02b89ecd72
frix241 <ariverosv@unsa.edu.pe>
2025-06-29 23:08:44 -0500

    Implementando la logica de game y su visualizacion html

--- juego-ahorcado/src/app/game/game.ts
+++ juego-ahorcado/src/app/game/game.ts
@@ -6,6 +6,60 @@ import { Component } from '@angular/core';
   templateUrl: './game.html',
   styleUrl: './game.css'
 })
-export class Game {
+export class Game implements OnInit {
+  displayWord = '';
+  guessedLetters: string[] = [];
+  wrongGuesses = 0;
+  maxWrongGuesses = 6;
+  gameOver = false;
+  gameWon = false;
 
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  constructor(private hangmanService: HangmanService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.startGame();
+  }
+
+  startGame(): void {
+    this.hangmanService.startNewGame();
+    this.updateState();
+    this.gameOver = false;
+    this.gameWon = false;
+  }
+
+  guess(letter: string): void {
+    if (this.gameOver) return;
+
+    const correct = this.hangmanService.guessLetter(letter);
+    this.updateState();
+
+    if (this.hangmanService.isGameWon()) {
+      this.router.navigate(['/result'], {
+        state: {
+          won: true,
+          word: this.hangmanService.getCurrentWord()
+        }
+      });
+    } else if (this.hangmanService.isGameLost()) {
+      this.router.navigate(['/result'], {
+        state: {
+          won: false,
+          word: this.hangmanService.getCurrentWord()
+        }
+      });
+    }
+  }
+
+  updateState(): void {
+    this.displayWord = this.hangmanService.getDisplayWord();
+    this.guessedLetters = this.hangmanService.getGuessedLetters();
+    this.wrongGuesses = this.hangmanService.getWrongGuesses();
+    this.maxWrongGuesses = this.hangmanService.getMaxWrongGuesses();
+  }
+
+  isLetterDisabled(letter: string): boolean {
+    return this.guessedLetters.includes(letter);
+  }
 }
