126e6aac3764f19aef476ce1d4182d1541566265
frix241 <ariverosv@unsa.edu.pe>
2025-06-29 23:19:12 -0500

    Corregido errores de importe

--- juego-ahorcado/src/app/game/game.ts
+++ juego-ahorcado/src/app/game/game.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { HangmanService } from '../services/hangman';
 @Component({
   selector: 'app-game',
   imports: [],
